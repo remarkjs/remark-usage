90342c76c4d95e61ee2fd84a9a166edb23bb331d
Titus Wormer <tituswormer@gmail.com>
2023-09-25 09:09:52 +0200

    Refactor `tsconfig.json`

--- test/fixtures/fail-could-not-run-example/example.js
+++ test/fixtures/fail-could-not-run-example/example.js
@@ -1,4 +1,5 @@
 // Require `pi`:
 var pi = require('./index.js');
 
+// @ts-expect-error: runtime.
 pi.s.o.m.e.i.n.v.a.l.i.d.j.a.v.a.s.c.r.i.p.t;
--- test/fixtures/ignore-lines/example.js
+++ test/fixtures/ignore-lines/example.js
@@ -7,7 +7,11 @@ var one = 1
 // remark-usage-ignore-next
 var two = sum(1, 1)
 
-// remark-usage-ignore-next 3
+// remark-usage-ignore-next 7
+/**
+ * @param {number} a
+ * @param {number} b
+ */
 function sum(a, b) {
   return a + b
 }
