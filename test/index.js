b46d8f5ee56b1e675e741ddf6b526e8dd22fa5c5
Titus Wormer <tituswormer@gmail.com>
2023-09-25 12:46:33 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 /**
- * @typedef {import('../index.js').Options} Options
+ * @typedef {import('remark-usage').Options} Options
  */
 
 /**
@@ -16,11 +16,11 @@ import fs from 'node:fs/promises'
 import test from 'node:test'
 import {fileURLToPath} from 'node:url'
 import {remark} from 'remark'
-import remarkUsage from '../index.js'
+import remarkUsage from 'remark-usage'
 
 test('remark-usage', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('remark-usage')).sort(), [
       'default'
     ])
   })
